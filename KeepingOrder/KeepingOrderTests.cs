f6443b6a1ea0f837fe4d9f92a375ff940ecba77e
jsteward <ptsteward1@yahoo.com>
2017-01-29 08:22:34 -0700

    Cleaned up 'KeepingOrder' project

---
+++ KeepingOrder/KeepingOrderTests.cs
@@ -0,0 +1,67 @@
+﻿namespace KeepingOrder {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using NUnit.Framework;    
+
+    [TestFixture]
+    public class KeepingOrderTests {
+
+        [Test]
+        public async Task Given_List_Of_Unprocessed_Items_Should_Return_Processed_Items_In_SameOrder() {
+            var data = Enumerable.Range(0, 100).Select(x => new Job(x, x)).ToList();
+            var pipeline = new OrderedPipeline();
+
+            foreach (var item in data) {
+                await pipeline.SendAsync(item);
+            }
+            pipeline.Complete();
+            await pipeline.Completion;
+            
+            Assert.IsTrue(Enumerable.SequenceEqual<Entity>(data, pipeline.FinalList));
+        }
+        
+    }
+
+    public class Job : Entity {
+
+        public Job(int id, int startValue) : base(id) {            
+            this.StartValue = startValue;
+        }
+        
+        public int StartValue { get; }        
+    }
+
+    public class JobComplete : Entity {
+
+        public JobComplete(int id, int endValue) : base(id) {
+            this.EndValue = endValue;
+        }
+
+        public int EndValue { get; }
+    }
+
+    public abstract class Entity : IEquatable<Entity> {
+
+        public Entity(int id) {
+            this.Id = id;
+        }
+
+        public virtual int Id { get; }
+
+        public bool Equals(Entity other) {
+            if (this.Id != other.Id) {
+                Console.WriteLine($"**{this.Id},{other.Id}**");
+            }
+            return this.Id == other.Id;
+        }
+
+        public override bool Equals(object obj) {
+            return Equals((Entity)obj);
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode() * Id.GetHashCode();
+        }
+    }
+}
---
+++ KeepingOrder/OrderedPipeline.cs
@@ -0,0 +1,83 @@
+﻿namespace KeepingOrder {
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+
+    public class OrderedPipeline {
+
+        private static int scale = 10;
+        private static Random rnd = new Random();
+
+        private BufferBlock<Job> buffer;
+        private TransformBlock<Job, Job> step1;
+        private TransformBlock<Job, JobComplete> step2;
+        private IObservable<JobComplete> finalStep;
+        private IDisposable finalStepSubscription;
+        
+        public ConcurrentQueue<JobComplete> FinalList { get; }
+
+        private TaskCompletionSource<bool> tcs;
+        public Task Completion { get; }
+
+        public OrderedPipeline() {
+            CreatePipeline();
+            LinkPipeline();
+            FinalList = new ConcurrentQueue<JobComplete>();
+            tcs = new TaskCompletionSource<bool>();
+            Completion = tcs.Task;
+        }        
+
+        public void Complete() {
+            buffer.Complete();          
+        }
+
+        public Task<bool> SendAsync(Job job) {
+            return buffer.SendAsync(job);
+        }
+
+        private void CreatePipeline() {
+            buffer = new BufferBlock<Job>(new DataflowBlockOptions() {
+                BoundedCapacity = 20
+            });
+
+            step1 = new TransformBlock<Job, Job>(x => {
+                Task.Delay(rnd.Next(100, 500)).Wait();
+                Console.WriteLine($"Step1,{x.Id}");
+                return new Job(x.Id, x.StartValue * scale);
+            }, new ExecutionDataflowBlockOptions() {
+                MaxDegreeOfParallelism = 8,
+                BoundedCapacity = 20,
+                EnsureOrdered = true
+            });
+
+            step2 = new TransformBlock<Job, JobComplete>(x => {
+                Task.Delay(rnd.Next(100, 500)).Wait();
+                Console.WriteLine($"Step2,{x.Id}");
+                return new JobComplete(x.Id, x.StartValue + scale);
+            }, new ExecutionDataflowBlockOptions() {
+                MaxDegreeOfParallelism = 8,
+                BoundedCapacity = 20,
+                EnsureOrdered = true
+            });
+        }     
+
+        private void LinkPipeline() {
+            var linkOptions = new DataflowLinkOptions() {
+                PropagateCompletion = true
+            };
+
+            buffer.LinkTo(step1, linkOptions);
+            step1.LinkTo(step2, linkOptions);
+            finalStep = step2.AsObservable();
+            var finalStepSubscription = finalStep.Subscribe(
+                x => {
+                    Task.Delay(rnd.Next(100, 500)).Wait();
+                    Console.WriteLine($"Final,{x.Id}");
+                    FinalList.Enqueue(x);
+                }, () => {
+                    tcs.SetResult(true);
+                });
+        }
+    }
+}
