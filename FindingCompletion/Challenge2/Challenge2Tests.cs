504f95b33ce981a82ec859b569c3243a4338c4f6
jsteward <ptsteward1@yahoo.com>
2017-01-29 16:26:29 -0700

    adding multiple producer tests

---
+++ FindingCompletion/Challenge2/Challenge2Tests.cs
@@ -0,0 +1,43 @@
+﻿namespace FindingCompletion.MultipleProducers {
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;    
+    using System.Threading.Tasks.Dataflow;
+
+    using FindingCompletion.MultipleProducers.Failing;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Challenge2Tests {
+
+        [Test]
+        public async Task All_Producers_Are_Complete_When_Pipeline_Completes() {
+            var rnd = new Random();
+            var conditionalBranchingFlow = new MultipleProducersFlow();
+            var messages = Enumerable.Range(0, 20).Select(x => new Message() {
+                Id = x,
+                Data = rnd.Next(-100, 100),
+                ExceededBounds = false
+            });
+
+            foreach (var message in messages) {
+                conditionalBranchingFlow.Post(message);
+            }
+            conditionalBranchingFlow.Complete();            
+
+            await conditionalBranchingFlow.FlowCompletion;
+                        
+            Assert.IsTrue(BothBranchesAreComplete(), PrintWhichBranchIsIncomplete());
+        }
+
+        private bool BothBranchesAreComplete() {
+            return MultipleProducersFlow.NegativeProducerCompletion.IsCompleted &&
+                   MultipleProducersFlow.PositiveProducerCompletion.IsCompleted;
+        }
+
+        private string PrintWhichBranchIsIncomplete() {
+            return MultipleProducersFlow.NegativeProducerCompletion.IsCompleted ? "Positive branch incomplete" : "Negative branch incomplete";
+        }
+    }
+}
